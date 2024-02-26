5a9b5319b67d9bf6040bafa47b84b5033ba07d11
harshulk47 <harshulk47@gmail.com>
2024-02-26 17:00:29 +0530

    ED-3243 update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
+import { NextFederationPlugin } from "@module-federation/nextjs-mf";
+// process.env.NEXT_PRIVATE_LOCAL_WEBPACK = true
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    const { isServer } = options;
+    config.experiments = { topLevelAwait: true };
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "shop",
+        remotes: {
+          main: `main@http://localhost:3000/_next/static/${
+            isServer ? "ssr" : "chunks"
+          }/remoteEntry.js`,
+        },
+        filename: "static/chunks/primaryEntry.js",
+      })
+    );
+    return config;
+  },
+};
+// module.exports = nextConfig;
 export default nextConfig;
